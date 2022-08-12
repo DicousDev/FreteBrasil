b8d678a4c2d422664058f89913bdb3eef37b0fec
João Victor Duarte <joaodicu387@gmail.com>
2022-08-12 18:50:21 -0300

    Create

---
+++ src/main/java/com/cd2tec/freteBrasil/FreteBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.cd2tec.freteBrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteBrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cd2tec/freteBrasil/FreteBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd2tec.freteBrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
